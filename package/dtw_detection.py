a0b7e89009892a3028a64c509d81101cdd3239ad
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-13 00:17:09 +0100

    Update dtw_detection.py

--- package/dtw_detection.py
+++ package/dtw_detection.py
@@ -24,7 +24,7 @@ def steps_detection(data, data_other_side, foot, freq):
 
     cost = matrix_cost(x, z, gyr_ok, acc_ok)
 
-    pic_correl_start = indexes(cost, 0.35, min_dist=len(gyr_ok) // 2, thres_abs=True)
+    pic_correl_start = find_stride.indexes(cost, 0.35, min_dist=len(gyr_ok) // 2, thres_abs=True)
     pic_correl_start = pic_correl_start[np.argsort(-cost[pic_correl_start])]
     F = [0] * len(x)  # same size as the signal, allows for counting if the steps are identified.
 
@@ -134,101 +134,3 @@ def matrix_cost(x, z, gyr_ok, jerk_ok, mu=0.1):
             matrix_2[j] = -abs(w_2) / 10
 
     return np.array(matrix, dtype=float)
-
-
-def indexes(y, thres=0.3, min_dist=1, thres_abs=False):
-    """Peak detection routine.
-
-    Finds the numeric index of the peaks in *y* by taking its first order difference. By using
-    *thres* and *min_dist* parameters, it is possible to reduce the number of
-    detected peaks. *y* must be signed.
-
-    Parameters
-    ----------
-    y : ndarray (signed)
-        1D amplitude data to search for peaks.
-    thres : float between [0., 1.]
-        Normalized threshold. Only the peaks with amplitude higher than the
-        threshold will be detected.
-    min_dist : int
-        Minimum distance between each detected peak. The peak with the highest
-        amplitude is preferred to satisfy this constraint.
-    thres_abs: boolean
-        If True, the thres value will be interpreted as an absolute value, instead of
-        a normalized threshold.
-
-    Returns
-    -------
-    ndarray
-        Array containing the numeric indexes of the peaks that were detected.
-        When using with Pandas DataFrames, iloc should be used to access the values at the returned positions.
-    """
-    if isinstance(y, np.ndarray) and np.issubdtype(y.dtype, np.unsignedinteger):
-        raise ValueError("y must be signed")
-
-    if not thres_abs:
-        thres = thres * (np.max(y) - np.min(y)) + np.min(y)
-
-    min_dist = int(min_dist)
-
-    # compute first order difference
-    dy = np.diff(y)
-    # print("dy", dy)
-
-    # propagate left and right values successively to fill all plateau pixels (0-value)
-    # print("where", np.where(dy == 0))
-    zeros, = np.where(dy == 0)
-
-    # check if the signal is totally flat
-    if len(zeros) == len(y) - 1:
-        print("Signal seems to be flat !")
-        return np.array([])
-
-    if len(zeros):
-        # compute first order difference of zero indexes
-        zeros_diff = np.diff(zeros)
-        # check when zeros are not chained together
-        zeros_diff_not_one, = np.add(np.where(zeros_diff != 1), 1)
-        # make an array of the chained zero indexes
-        zero_plateaus = np.split(zeros, zeros_diff_not_one)
-
-        # fix if leftmost value in dy is zero
-        if zero_plateaus[0][0] == 0:
-            dy[zero_plateaus[0]] = dy[zero_plateaus[0][-1] + 1]
-            zero_plateaus.pop(0)
-
-        # fix if rightmost value of dy is zero
-        if len(zero_plateaus) and zero_plateaus[-1][-1] == len(dy) - 1:
-            dy[zero_plateaus[-1]] = dy[zero_plateaus[-1][0] - 1]
-            zero_plateaus.pop(-1)
-
-        # for each chain of zero indexes
-        for plateau in zero_plateaus:
-            median = np.median(plateau)
-            # set leftmost values to leftmost non zero values
-            dy[plateau[plateau < median]] = dy[plateau[0] - 1]
-            # set rightmost and middle values to rightmost non zero values
-            dy[plateau[plateau >= median]] = dy[plateau[-1] + 1]
-
-    # find the peaks by using the first order difference
-    peaks = np.where(
-        (np.hstack([dy, 0.0]) < 0.0)
-        & (np.hstack([0.0, dy]) > 0.0)
-        & (np.greater(y, thres))
-    )[0]
-
-    # handle multiple peaks, respecting the minimum distance
-    if peaks.size > 1 and min_dist > 1:
-        highest = peaks[np.argsort(y[peaks])][::-1]
-        rem = np.ones(y.size, dtype=bool)
-        rem[peaks] = False
-
-        for peak in highest:
-            if not rem[peak]:
-                sl = slice(max(0, peak - min_dist), peak + min_dist + 1)
-                rem[sl] = True
-                rem[peak] = False
-
-        peaks = np.arange(y.size)[~rem]
-
-    return peaks
