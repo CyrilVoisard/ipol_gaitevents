8865d9aaf6279a63507823d3e58ed478d7123a14
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-13 00:22:56 +0100

    Update seg_detection.py

--- package/seg_detection.py
+++ package/seg_detection.py
@@ -16,12 +16,12 @@ def disp_seg(seg):
     print(seg)
 
 
-def plot_segdetection_xs(data_tr, steps_lim, id_exp=0, freq=100, download=False, output_files=0):
+def plot_segdetection_xs(data_lb, steps_lim, id_exp=0, freq=100, download=False, output_files=0):
     # segmentation
-    seg = seg_detection(data_tr, steps_lim, id_exp=id_exp, freq=freq, download=download, output_files=output_files)
+    seg = seg_detection(data_lb, steps_lim, id_exp=id_exp, freq=freq, download=download, output_files=output_files)
 
     # Graphic signals
-    t_full, angle_x_full = signals_for_seg(data_tr, steps_lim)
+    t_full, angle_x_full = signals_for_seg(data_lb, steps_lim)
 
     # Figure initialisation et signal brut
     plt.rcParams["figure.figsize"] = (20, 10)
@@ -51,63 +51,58 @@ def plot_segdetection_xs(data_tr, steps_lim, id_exp=0, freq=100, download=False,
     return seg
 
 
-def seg_detection(data_tr, steps_lim, id_exp=0, freq=100, download=False, output_files=0):
+def seg_detection(data_lb, steps_lim, freq):
     start = int(np.min(steps_lim["TO"]))
     end = int(np.max(steps_lim["HS"]))
-    # Useful signals
-    t_full, angle_x_full = signals_for_seg(data_tr, steps_lim)
+    # useful signals
+    t_full, angle_x_full = signals_for_seg(data_lb, steps_lim)
 
-    # Middle argument
+    # middle argument
     mid_index = find_nearest(angle_x_full, 90)
     l_search = min(abs(end - mid_index), abs(start - mid_index))
     start = int(mid_index - l_search)
     end = int(mid_index + l_search)
 
-    t = data_tr["PacketCounter"][start:end]
+    t = data_lb["PacketCounter"][start:end]
     angle_x = angle_x_full[start:end]
 
     L = len(t)
-    # Affine approximation of go phase
+    # affine approximation of go phase
     a_go, b_go, r_go, p_value_go, std_err_go = linregress(t[0:2*L//5], angle_x[0:2*L//5])
-    # Affine approximation of back phase
+    # affine approximation of back phase
     a_back, b_back, r_back, p_value_back, std_err_back = linregress(t[3*L//5:], angle_x[3*L//5:])
-    # Affine approximation of U-turn phase
+    # affine approximation of U-turn phase
     a_u, b_u, r_u, p_value_u, std_err_u = linregress(t_full[mid_index - freq:mid_index + freq],
                                                      angle_x_full[mid_index - freq:mid_index + freq])
 
-    # Intersection points
+    # intersection points
     x_inter_go = (b_go - b_u) / (a_u - a_go)
     x_inter_back = (b_back - b_u) / (a_u - a_back)
     approx_seg_lim = [start, freq * x_inter_go, freq * x_inter_back, end]
 
     # U-Turn boundaries
-    # strT = ft.stride_time(data_tr, approx_seg_lim, steps_lim, freq=freq)
+    # strT = ft.stride_time(data_lb, approx_seg_lim, steps_lim, freq=freq)
     # print(x_inter_go, strT, start, approx_seg_lim)
     # start_uturn = 100 * (x_inter_go - strT) + np.argmin(
     # angle_x[int(100 * x_inter_go - 100 * strT - start):int(100 * x_inter_go - start)])
     # end_uturn = int(100 * x_inter_back) + np.argmax(
     # angle_x[int(100 * x_inter_back - start):int(100 * x_inter_back + 100 * strT - start)])
 
-    # seg = [0, start_uturn, end_uturn, len(data_tr["Gyr_X"])]
-    seg = [0, int(100*x_inter_go), int(100*x_inter_back), len(data_tr["Gyr_X"])]
-
-    if download:
-        os.chdir(output_files)
-        name_file = id_exp + '_auto_seg.txt'
-        np.savetxt(name_file, seg, delimiter=';', fmt='%d')
+    # seg = [0, start_uturn, end_uturn, len(data_lb["Gyr_X"])]
+    seg = [0, int(100*x_inter_go), int(100*x_inter_back), len(data_lb["Gyr_X"])]
 
     return seg
 
 
-def signals_for_seg(data_tr, steps_lim):
+def signals_for_seg(data_lb, steps_lim):
 
-    gyr_x = data_tr['Gyr_X']
+    gyr_x = data_lb['Gyr_X']
     angle_x_full = np.cumsum(gyr_x)
     a = np.median(angle_x_full[0:len(angle_x_full) // 2])  # Tout début du signal
     z = np.median(angle_x_full[len(angle_x_full) // 2:len(angle_x_full)])  # Fin du signal
 
     angle_x_full = np.sign(z) * (angle_x_full - a) * 180 / abs(z)
-    t_full = data_tr["PacketCounter"]
+    t_full = data_lb["PacketCounter"]
 
     return t_full, angle_x_full
 
