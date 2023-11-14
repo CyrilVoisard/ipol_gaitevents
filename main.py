b6320b54756e35fdc412b6157b7f90a68c723696
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-14 22:59:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -127,9 +127,9 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description='Return a semiogram for a given trial.')
-    parser.add_argument('-i1', metavar='data_lb', help='Time series for the lower back sensor.')
-    parser.add_argument('-i2', metavar='data_rf', help='Time series for the right foot sensor.')
-    parser.add_argument('-i0', metavar='data_lf', help='Time series for the left foot sensor.')
+    parser.add_argument('-i0', metavar='data_lb', help='Time series for the lower back sensor.')
+    parser.add_argument('-i1', metavar='data_rf', help='Time series for the right foot sensor.')
+    parser.add_argument('-i2', metavar='data_lf', help='Time series for the left foot sensor.')
 
     
     parser.add_argument('-freq', metavar='freq',
@@ -139,9 +139,9 @@ if __name__ == "__main__":
     freq = int(args.freq)
     
     # load data
-    data_lb = import_data.import_XSens(os.path.join(data_WD, args.i1))
-    data_rf = import_data.import_XSens(os.path.join(data_WD, args.i2))
-    data_lf = import_data.import_XSens(os.path.join(data_WD, args.i0))
+    data_lb = import_data.import_XSens(os.path.join(data_WD, args.i0))
+    data_rf = import_data.import_XSens(os.path.join(data_WD, args.i1))
+    data_lf = import_data.import_XSens(os.path.join(data_WD, args.i2))
     
     # gait events and steps detection
     steps_rf, steps_lf, steps_lim_full = dtw_detection.steps_detection_full(data_rf, data_lf, freq)
