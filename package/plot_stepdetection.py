01cd1c881ce35565c32a960c3a42b5e1c03c228b
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-15 00:10:50 +0100

    Update plot_stepdetection.py

--- package/plot_stepdetection.py
+++ package/plot_stepdetection.py
@@ -140,5 +140,5 @@ def plot_stepdetection(steps_rf, steps_lf, data_rf, data_lf, freq, output, corre
 
     # save the fig
     path_out = os.path.join(output, "steps.svg")
-    plt.savefig(path_out, dpi=100,
+    plt.savefig(path_out, dpi=80,
                     transparent=True, bbox_inches="tight")
