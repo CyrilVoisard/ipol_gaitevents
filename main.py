160811f120291366fa27cc720b50a691c4dd3f41
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-14 23:16:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def print_quality_index(steps_lim_full, seg_lim):
     else :
         ax.annotate(qi,xy=( 1.18*np.pi, .5),color=colormap(.05+0.90*(qi/max_qi)),fontsize=50)
 
-    path_out = os.path.join(data_WB, "quality_index.svg")
+    path_out = os.path.join(data_WD, "quality_index.svg")
     plt.savefig(path_out, dpi=100,
                     transparent=True, bbox_inches="tight")
     
