3049b5cdd9a3e14b374fb60554e0711f82bd8658
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-15 00:21:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -68,21 +68,24 @@ def print_seg_detection(seg_lim, freq):
                     'U-Turn end': seg_lim[2],
                     'End': seg_lim[3]}
 
-    display_dict = {'Start_title': "Start:",
-                    'Start': "Start: {Start}".format(**seg_lim_dict),
-                    'Start_sec': "{}".format(round(seg_lim_dict['Start']/100)),
+    display_dict = {'Start_title': "Trial start",
+                    'Start': "{Start}".format(**seg_lim_dict),
+                    'Start_sec': "{}".format(round(seg_lim_dict['Start']/100, 2)),
+                    'U-Turn start_title': "U-turn start",
                     'U-Turn start': "U-Turn start: {U-Turn start}".format(**seg_lim_dict),
-                    'U-Turn start_sec': "{}".format(round(seg_lim_dict['U-Turn start']/100)),
+                    'U-Turn start_sec': "{}".format(round(seg_lim_dict['U-Turn start']/100, 2)),
+                    'U-Turn end_title': "U-turn end",
                     'U-Turn end': "U-Turn_end: {U-Turn end}".format(**seg_lim_dict),
-                    'U-Turn end_sec': "{}".format(round(seg_lim_dict['U-Turn end']/100)),
+                    'U-Turn end_sec': "{}".format(round(seg_lim_dict['U-Turn end']/100, 2)),
+                    'End_title': "Trial end",
                     'End': "End: {End}".format(**seg_lim_dict), 
                     'End_sec': "{}".format(round(seg_lim_dict['End']/100))}
         
     info_msg = """
-    {Start_title:<15}| {Start_sec:<15}| {Start_sec:<15}
-    {U-Turn start:<15}| {U-Turn start_sec:<15}| {Start_sec:<15}
-    {U-Turn end:<15}| {U-Turn end_sec:<15}| {Start_sec:<15}
-    {End:<15}| {End_sec:<15}| {Start_sec:<15}
+    {Start_title:<15}| {Start:<15}| {Start_sec:<15}
+    {U-Turn start_title:<15}| {U-Turn start:<15}| {U-Turn start_sec:<15}
+    {U-Turn end_title:<15}| {U-Turn end:<15}| {U-Turn end_sec:<15}
+    {End_title:<15}| {End:<15}| {End_sec:<15}
     """
 
     # Dump information
