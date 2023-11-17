fc345a65ffc77e9d006c4b0c52d23c7dfbe15155
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-17 18:54:05 +0100

    Update quality.py

--- package/quality.py
+++ package/quality.py
@@ -4,34 +4,6 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
-def compute_quality_index(steps_lim, seg_lim):
-  """Compute the quality index of the trial gait events detection (between 0 and 100)
-  Add quality index formula ? 
-
-    Parameters
-    ----------
-        steps_lim {dataframe} -- pandas dataframe with gait events
-        seg_lim {dataframe} -- pandas dataframe with phases events 
-
-    Returns
-    -------
-        qi {int} -- quality index 
-    """
-  
-  # estimation of stride alternation
-  steps_lim_sort = steps_lim.sort_values(by = ['HS', 'TO'])
-  alt_go = steps_lim_sort[steps_lim_sort['HS'] < seg_lim[1]]['Foot'].tolist()
-  alt_back = steps_lim_sort[steps_lim_sort['HS'] > seg_lim[2]]['Foot'].tolist()
-  i = 0
-  for k in range(len(alt_go)-1):
-      i = i + abs(alt_go[k+1]-alt_go[k])
-  for k in range(len(alt_back)-1):
-      i = i + abs(alt_back[k+1]-alt_back[k])
-  qi = round(100*i/(len(alt_go) + len(alt_back)-2))
-
-  return qi
-    
-
 def print_quality_index(steps_lim_full, seg_lim, output):
     """Compute the quality index of the trial gait events detection (between 0 and 100) and produce a picture of the number surrounded by an appropriately colored circle. 
     Add quality index formula ? 
@@ -52,13 +24,13 @@ def print_quality_index(steps_lim_full, seg_lim, output):
     path_raw = os.path.join(output, "quality_index_raw.svg")
     plot_quality_index(qi_raw, path_raw)
   
-    steps_lim_corrected = steps_lim_full
+    steps_lim_corrected, seg_lim_corrected = correct_steps_lim(steps_lim_full, seg_lim)
   
     qi_corrected = compute_quality_index(steps_lim_corrected, seg_lim)
     path_corrected = os.path.join(output, "quality_index_corrected.svg")
     plot_quality_index(qi_corrected, path_corrected)
   
-    return qi_corrected, steps_lim_corrected
+    return qi_corrected, steps_lim_corrected, seg_lim_corrected
 
 
 def plot_quality_index(qi, path):
@@ -90,3 +62,115 @@ def plot_quality_index(qi, path):
   
     plt.savefig(path, dpi=80,
                     transparent=True, bbox_inches="tight")
+
+
+def compute_quality_index(steps_lim, seg_lim):
+  """Compute the quality index of the trial gait events detection (between 0 and 100)
+  Add quality index formula ? 
+
+    Parameters
+    ----------
+        steps_lim {dataframe} -- pandas dataframe with gait events
+        seg_lim {dataframe} -- pandas dataframe with phases events 
+
+    Returns
+    -------
+        qi {int} -- quality index 
+    """
+  
+  # estimation of stride alternation
+  steps_lim_sort = steps_lim.sort_values(by = ['HS', 'TO'])
+  alt_go = steps_lim_sort[steps_lim_sort['HS'] < seg_lim[1]]['Foot'].tolist()
+  alt_back = steps_lim_sort[steps_lim_sort['HS'] > seg_lim[2]]['Foot'].tolist()
+  i = 0
+  for k in range(len(alt_go)-1):
+      i = i + abs(alt_go[k+1]-alt_go[k])
+  for k in range(len(alt_back)-1):
+      i = i + abs(alt_back[k+1]-alt_back[k])
+  qi = round(100*i/(len(alt_go) + len(alt_back)-2))
+
+  return qi
+    
+
+def correct_steps_lim(steps_lim, seg_lim):
+  """Correction of the steps_lim dataframe. U-turn steps 
+
+    Parameters
+    ----------
+        steps_lim {dataframe} -- pandas dataframe with gait events
+        seg_lim {dataframe} -- pandas dataframe with phases events 
+
+    Returns
+    -------
+        qi {int} -- quality index 
+    """
+  
+  # estimation of the start and end time samples for the trial to compute 
+  start, end = get_bornes(steps_lim, seg_lim)
+  seg_lim_corrected = [start, seg_lim[1], seg_lim[2], end]
+  
+  steps_lim_corrected = steps_lim
+
+  return steps_lim_corrected, seg_lim_corrected
+
+
+def get_bornes(steps_lim, seg_lim):
+  """Find start and end time sample for the trial. The beginning corresponds to the Toe-Off of the first step after deleting the steps outside the trial. 
+  The end corresponds to the Heel-Strike of the last step after deleting the steps outside the trial. 
+
+    Parameters
+    ----------
+        steps_lim {dataframe} -- pandas dataframe with gait events
+        seg_lim {dataframe} -- pandas dataframe with phases events 
+
+    Returns
+    -------
+        start, end {int, int} -- time samples corresponding to the estimated start and end for the trial. 
+    """
+  
+  start = np.Inf
+  end = 0
+
+  for foot in [0, 1]:
+
+      # estimation of average stride duration
+      steps_lim_f = steps_lim[steps_lim["Foot"] == foot]
+      hs_f = steps_lim_f["HS"].tolist()
+      hs_t = []
+    
+      for i in range(len(hs_f) - 1):  # for outlier search, we exclude the first and last steps 
+          hs_t.append(hs_f[i + 1] - hs_f[i])  # hs_t contains all stride durations 
+      hs_t = ft.rmoutliers(hs_t)  # function for eliminating outliers (start, U-turn, end)
+      strT = np.median(hs_t)  # estimation of average stride duration 
+      to_f = steps_lim_f["TO"].tolist()  # for the side under consideration, this vector contains all Toe-Off dates
+
+      # find the estimated start of the trial: start from the beginning and work forward
+      find_start = 0
+      i = 0
+      while not find_start:
+          if abs(to_f[i] - to_f[i + 2]) < 3 * strT:  # if the time between strides i and i+2 (expected time 2*strT) is less than 3*strT, the test is considered to have started
+              find_start = 1
+              if to_f[i] < start:
+                  start = to_f[i]
+          else:  # if the previous condition is not met, stride i is probably an outlier before the trial begins.
+              i = i + 1
+
+      # search for the estimated end of the trial: start from the end and work backwards
+      find_end = 0
+      i = np.argmin(abs(np.array(hs_f) - (seg_lim.iloc[2, 0] + 0.5 * (seg_lim.iloc[2, 0] - start))))
+
+      while (not find_end) & (i < len(hs_f)):
+          if abs(hs_f[i] - hs_f[i - 2]) < 3 * strT:  # si la durée entre les strides i et i+2 (durée attendue 2*strT) est inférieure 3*strT, on considère qu'on est encore dans l'épreuve
+              if i == len(hs_f) - 1:
+                  find_end = 1
+                  if hs_f[i] > end:
+                      end = hs_f[i]
+              else:
+                  i = i + 1
+          else:  # if the previous condition is not met, stride i is probably an outlier after the end of the trial. We therefore set the end of the trial at i-1
+              find_end = 1
+              if hs_f[i - 1] > end:
+                  end = hs_f[i - 1]
+
+  return start, end
+
