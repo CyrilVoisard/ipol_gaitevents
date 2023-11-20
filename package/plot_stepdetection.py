f6f490cf3cd99539f615911ad5fd3e98693ed9b2
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-20 01:27:43 +0100

    Update plot_stepdetection.py

--- package/plot_stepdetection.py
+++ package/plot_stepdetection.py
@@ -14,7 +14,7 @@ def plot_stepdetection(steps_rf, steps_lf, data_rf, data_lf, freq, output, corre
   
     name = "Gait events detection - "
 
-    fig, ax = plt.subplots(3, figsize=(20, 12), sharex=True, sharey=False, height_ratios=[10, 1, 10])
+    fig, ax = plt.subplots(3, figsize=(20, 12), sharex=True, sharey=False, gridspec_kw={'height_ratios':[10,1,10]})
 
     ax[0].grid()
     ax[2].grid()
