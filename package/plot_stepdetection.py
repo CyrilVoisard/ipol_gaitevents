b21a416e33dfdeb817cd6976ebfb528306a14d52
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-20 13:05:56 +0100

    Update plot_stepdetection.py

--- package/plot_stepdetection.py
+++ package/plot_stepdetection.py
@@ -8,9 +8,9 @@ import os
 from package import deal_stride
 
 
-def plot_stepdetection(steps_rf, steps_lf, data_rf, data_lf, freq, output, corrected=False):
-    #steps_rf = steps_lim[steps_lim['Foot']==1]
-    #steps_lf = steps_lim[steps_lim['Foot']==0]
+def plot_stepdetection(steps_lim, data_rf, data_lf, freq, output):
+    steps_rf = steps_lim[(steps_lim['Foot']==1) & (steps_lim['Correct']==1)].to_numpy()
+    steps_lf = steps_lim[(steps_lim['Foot']==0) & (steps_lim['Correct']==1)].to_numpy()
   
     name = "Gait events detection - "
 
