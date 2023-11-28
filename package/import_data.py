138c63950222636f2b0500394cb955d5cc390968
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-28 17:24:01 +0100

    Update import_data.py

--- package/import_data.py
+++ package/import_data.py
@@ -87,6 +87,7 @@ def filter_sig(data, type_sig, order, fc):
     Pandas dataframe
         data
     """
+    
     data[type_sig + "_X"] = low_pass_filter(data[type_sig + "_X"], order, fc)
     data[type_sig + "_Y"] = low_pass_filter(data[type_sig + "_Y"], order, fc)
     data[type_sig + "_Z"] = low_pass_filter(data[type_sig + "_Z"], order, fc)
@@ -94,109 +95,3 @@ def filter_sig(data, type_sig, order, fc):
     return data
 
 
-def low_pass_filter(sig, order=8, fc=14, fe=100):
-    """Definition of a Butterworth low-pass filter
-
-    Arguments:
-        sig {dataframe} -- pandas dataframe
-        order {int} -- order of the Butterworth low-pass filter
-        fc {int} -- cut-off frequency of the Butterworth low-pass filter
-        fe {int} -- acquisition frequency for the data
-    Returns
-    -------
-    ndarray
-        filter
-    """
-    
-    f_nyq = fe / 2.  # Hz
-
-    # definition of the Butterworth low-pass filter
-    (b, a) = butter(N=order, Wn=(fc / f_nyq), btype='low', analog=False)
-
-    # application
-    return filtfilt(b, a, sig)
-
-
-# steps
-def get_steps(filename, seg_lim): 
-    """Gets the gait events segmentation from the metadata JSON file. 
-
-    Arguments:
-        filename {str} -- File path
-        
-    Returns
-    -------
-    Pandas dataframe
-        steps_lim
-    """
-    with open(filename) as steps_file:
-        steps_dict = json.load(steps_file)
-    steps_left = steps_dict["LeftFootEvents"]
-    steps_right = steps_dict["RightFootEvents"]
-
-    foot = []
-    to = []
-    hs = []
-
-    for i in range(len(steps_right)):
-        if inside(steps_right[i][0], steps_right[i][1], seg_lim):
-            foot.append(1)
-            to.append(steps_right[i][0])
-            hs.append(steps_right[i][1])
-
-    for i in range(len(steps_left)):
-        if inside(steps_left[i][0], steps_left[i][1], seg_lim):
-            foot.append(0)
-            to.append(steps_left[i][0])
-            hs.append(steps_left[i][1])
-
-    _dict = {'Foot': foot, 'TO': to, 'HS': hs}
-    type_dict = {'Foot': int, 'HS': int, 'TO': int}
-    steps_lim = pd.DataFrame(_dict)
-    steps_lim = steps_lim.astype(type_dict)
-    
-    return steps_lim
-    
-
-# phases
-def get_seg(filename): 
-    """Gets the gait events segmentation from the metadata JSON file. 
-
-    Arguments:
-        filename {str} -- File path
-        
-    Returns
-    -------
-    Pandas dataframe
-        seg_lim
-    """
-    with open(filename) as seg_file:
-        seg_dict = json.load(seg_file)
-    [start_u, end_u] = seg_dict["UTurnBoundaries"]
-    start = min(np.min(seg_dict["LeftFootEvents"]), np.min(seg_dict["RightFootEvents"]))
-    end = max(np.max(seg_dict["LeftFootEvents"]), np.max(seg_dict["RightFootEvents"]))
-    return pd.DataFrame([start, start_u, end_u, end])
-    
-
-def inside(to, hs, seg_lim):
-    """Check whether the considered step is valid, meaning it is not included in the half-turn.
-
-    Arguments:
-        to {int} -- Toe-Off time
-        hs {int} -- Heel-Strike time
-        seg_lim {panda DataFrame} -- boundaries of the trial
-        
-    Returns
-    -------
-    Boolean
-        in_
-    """
-    seg_lim = pd.DataFrame(seg_lim)
-    if (hs <= seg_lim.iloc[1, 0]) & (to <= seg_lim.iloc[1, 0]):
-            in_ = True
-    else:
-        if (hs >= seg_lim.iloc[2, 0]) & (to >= seg_lim.iloc[2, 0]):
-            in_ = True
-        else:
-            in_ = False
-    return in_
