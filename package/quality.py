41bedc7830bb29143acebd27aeb565e9714f530c
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-12-06 15:57:32 +0100

    Update quality.py

--- package/quality.py
+++ package/quality.py
@@ -22,15 +22,18 @@ def print_all_quality_index(q1, q2, q3, output):
         steps_lim_corrected {dataframe} -- pandas dataframe with gait events after elimination of the extra trial steps
     """
 
-    q_mean = 1/3 * (np.mean(q1) + np.mean(q2) + np.mean(q3))
+    q_mean = round(1/3 * (np.mean(q1) + np.mean(q2) + np.mean(q3)), 0)
 
     fig = plt.figure(figsize=(6, 5))
     gs = GridSpec(nrows=3, ncols=2, width_ratios = [3, 1])
     ax0 = fig.add_subplot(gs[:, 0], projection='polar')
     ax0 = plot_quality_index(q_mean, ax0)
     ax1 = fig.add_subplot(gs[0, 1], projection='polar')
-    ax1 = fig.add_subplot(gs[1, 1], projection='polar')
-    ax1 = fig.add_subplot(gs[2, 1], projection='polar')
+    ax1 = plot_quality_index(round(np.mean(q1), 0), ax0)
+    ax2 = fig.add_subplot(gs[1, 1], projection='polar')
+    ax2 = plot_quality_index(round(np.mean(q2), 0), ax0)
+    ax3 = fig.add_subplot(gs[2, 1], projection='polar')
+    ax3 = plot_quality_index(round(np.mean(q3), 0), ax0)
 
     path = os.path.join(output, "quality_index_raw.svg")
 
