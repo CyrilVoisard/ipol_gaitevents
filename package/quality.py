dee6d09025cc6949cf0907f0265c3777da95ee83
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-12-07 15:32:15 +0100

    Update quality.py

--- package/quality.py
+++ package/quality.py
@@ -28,15 +28,15 @@ def print_all_quality_index(q1, q2, q3, output):
     gs = GridSpec(nrows=3, ncols=2, width_ratios = [3, 1])
     ax0 = fig.add_subplot(gs[:, 0], projection='polar')
     ax0 = plot_quality_index(q_mean, ax0, scale = 1)
-    ax0.text(0.22, 0.75, 'Global quality score', fontsize = 14, fontweight='bold', transform=plt.gcf().transFigure)
+    ax0.text(0.22, 0.79, 'Global quality score', fontsize = 14, fontweight='bold', transform=plt.gcf().transFigure)
     ax1 = fig.add_subplot(gs[0, 1], projection='polar')
-    ax1 = plot_quality_index(round(np.mean(q1)), ax1, scale = 5)
+    ax1 = plot_quality_index(round(np.mean(q1)), ax1, scale = 3)
     ax1.text(0.7, 0.88, 'Protocol quality score', fontsize = 9, fontweight='bold', transform=plt.gcf().transFigure)
     ax2 = fig.add_subplot(gs[1, 1], projection='polar')
-    ax2 = plot_quality_index(round(np.mean(q2)), ax2, scale = 5)
+    ax2 = plot_quality_index(round(np.mean(q2)), ax2, scale = 3)
     ax2.text(0.7, 0.6, 'Intrinsec quality score', fontsize = 9, fontweight='bold', transform=plt.gcf().transFigure)
     ax3 = fig.add_subplot(gs[2, 1], projection='polar')
-    ax3 = plot_quality_index(round(np.mean(q3)), ax3, scale = 5)
+    ax3 = plot_quality_index(round(np.mean(q3)), ax3, scale = 3)
     ax3.text(0.7, 0.33, 'Extrinsec quality score', fontsize = 9, fontweight='bold', transform=plt.gcf().transFigure)
 
     path = os.path.join(output, "quality_index_raw.svg")
@@ -61,7 +61,7 @@ def plot_quality_index(q, ax, scale):
     # f, ax = plt.subplots(nrows=1, ncols=1, figsize=(4,4),subplot_kw=dict(projection='polar'))
     #Scatter version
     yval = np.ones_like(xval)
-    ax.scatter(xval, yval, c=xval, s=300/scale, cmap=colormap, norm=norm, linewidths=1)
+    ax.scatter(xval, yval, c=xval, s=300/(2*scale), cmap=colormap, norm=norm, linewidths=1)
   
     ax.set_axis_off()
     ax.set_ylim(0,1.5)
