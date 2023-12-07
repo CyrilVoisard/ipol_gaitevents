64d2516412d37b7429c5859ba83460eaf88cce5f
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-12-07 15:39:52 +0100

    Update seg_detection.py

--- package/seg_detection.py
+++ package/seg_detection.py
@@ -20,7 +20,7 @@ def plot_seg_detection(seg_lim, data_lb, regression, freq, output):
     ax.set_ylabel('Angular position (°)', fontsize=15)
     ax.set_xlabel('Time (s)', fontsize=15)
     ax.xaxis.set_tick_params(labelsize=12)
-    fig.suptitle('Mediolateral angular position: ', fontsize = 16, fontweight='bold')
+    fig.suptitle('Mediolateral angular position', fontsize = 16, fontweight='bold')
 
     # Phases segmentation delimitation 
     ax.vlines(seg_lim[1] / freq, -50, 230, 'red', '-', linewidth=2, label="$u_{go}$ and $u_{back}$")
