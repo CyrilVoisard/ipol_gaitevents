5deba305348db483153d9b1c8baf6226c50fd482
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-12-09 01:53:22 +0100

    Update download.py

--- package/download.py
+++ package/download.py
@@ -9,18 +9,16 @@ def json_report(seg_lim, steps_lim, freq, output):
     dict_events["TrialBoundaries"] = [int(seg_lim[0]), int(seg_lim[3])]
     dict_events["UTurnBoundaries"] = [int(seg_lim[1]), int(seg_lim[2])]
 
-    strides = steps_lim[steps_lim["Correct"] == 1]
-    print(strides)
-    strides_lf = []
-    strides_rf = []
-    for i in range(len(strides)):
-        print("i", i, strides["TO"][i])
-        if strides["Foot"][i]==1:
-            strides_rf.append([int(strides["TO"][i]), int(strides["HS"][i])])
-        else:
-            strides_lf.append([int(strides["TO"][i]), int(strides["HS"][i])])
-    dict_events["LeftFootEvents"] = strides_lf
-    dict_events["RightFootEvents"] = strides_rf
+    steps_lim_lf = []
+    steps_lim_rf = []
+    for i in range(len(steps_lim)):
+        if steps_lim["Corrected"][i]==1:
+            if steps_lim["Foot"][i]==1:
+                steps_lim_rf.append([int(steps_lim["TO"][i]), int(steps_lim["HS"][i])])
+            else:
+                steps_lim_lf.append([int(steps_lim["TO"][i]), int(steps_lim["HS"][i])])
+    dict_events["LeftFootEvents"] = steps_lim_lf
+    dict_events["RightFootEvents"] = steps_lim_rf
     
     with open(os.path.join(output, "gait_events.json"), "w") as f:
         json.dump(dict_events, f)
