a42dd233a6fc91ac669fe7efc82be733b4cfd77d
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-12-09 01:35:28 +0100

    Update download.py

--- package/download.py
+++ package/download.py
@@ -9,7 +9,7 @@ def json_report(seg_lim, steps_lim, freq, output):
     dict_events["TrialBoundaries"] = [int(seg_lim[0]), int(seg_lim[3])]
     dict_events["UTurnBoundaries"] = [int(seg_lim[1]), int(seg_lim[2])]
 
-    strides = steps_lim[steps_lim["Correct" == 1]]
+    strides = steps_lim[steps_lim["Correct"] == 1]
     strides_lf = []
     strides_rf = []
     for i in range(len(strides)):
