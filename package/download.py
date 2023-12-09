8be7eda8c767e98e3539dd28e897c6c3dabca912
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-12-09 01:32:41 +0100

    Update download.py

--- package/download.py
+++ package/download.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 
 
-def json_report(seg_lim_corrected, steps_lim_corrected, freq, output):
+def json_report(seg_lim, steps_lim, freq, output):
     dict_events = dict()
     dict_events["Detection date"] = datetime.now()
     dict_events["Freq"] = freq
     
-    dict_events["TrialBoundaries"] = [int(seg_lim_corrected[0]), int(seg_lim_corrected[3])]
-    dict_events["UTurnBoundaries"] = [int(seg_lim_corrected[1]), int(seg_lim_corrected[2])]
+    dict_events["TrialBoundaries"] = [int(seg_lim[0]), int(seg_lim[3])]
+    dict_events["UTurnBoundaries"] = [int(seg_lim[1]), int(seg_lim[2])]
 
-    strides = steps_lim_corrected[steps_lim_corrected["Correct"] == True]
+    strides = steps_lim[steps_lim["Correct" == 1]]
     strides_lf = []
     strides_rf = []
     for i in range(len(strides)):
