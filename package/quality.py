Fix fontsize calculation in quality.py annotations

package/quality.py
@@ -53,12 +53,12 @@ def plot_quality_index(q, output):
     ax.set_axis_off()
     ax.set_ylim(0,1.5)
     if q<10:
-        ax.annotate(q, xy=( 1.25*np.pi, .3), color=colormap(.05+0.90*(q/max_q)), fontsize=50/scale)
+        ax.annotate(q, xy=(1.25*np.pi, .3), color=colormap(.05+0.90*(q/max_q)), fontsize=50)
     else :
         if  q == 100: 
-            ax.annotate(q, xy=(1.11*np.pi, .7), color=colormap(.05+0.90*(q/max_q)), fontsize=50/scale)
+            ax.annotate(q, xy=(1.11*np.pi, .7), color=colormap(.05+0.90*(q/max_q)), fontsize=50)
         else:
-            ax.annotate(q, xy=(1.18*np.pi, .5), color=colormap(.05+0.90*(q/max_q)), fontsize=50/scale)
+            ax.annotate(q, xy=(1.18*np.pi, .5), color=colormap(.05+0.90*(q/max_q)), fontsize=50)
 
     fig.title('Quality score', fontsize = 14, fontweight='bold')
     path = os.path.join(output, "quality_index.svg")