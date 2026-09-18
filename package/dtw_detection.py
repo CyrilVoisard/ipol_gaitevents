9cb590c8dae4fcddbded194e8da9bc782e8a2e22
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2026-09-19 01:37:09 +0200

    Comment out stride index finding line
    Comment out the line that finds stride indexes in the cost matrix.

--- package/dtw_detection.py
+++ package/dtw_detection.py
@@ -58,7 +58,7 @@ def steps_detection(data_1, data_2, foot, freq, output):
     cost = matrix_cost(x, z, gyr_ref, jerk_ref)
     
     pic_correl_start, _ = find_peaks(cost, height=0.35, distance=max(1, len(gyr_ref) // 2))
-    pic_correl_start = find_stride.indexes(cost, 0.35, min_dist=len(gyr_ref) // 2, thres_abs=True)
+    # pic_correl_start = find_stride.indexes(cost, 0.35, min_dist=len(gyr_ref) // 2, thres_abs=True)
     pic_correl_start = pic_correl_start[np.argsort(-cost[pic_correl_start])]
     F = [0] * len(x)  # same size as the signal, allows for counting if the steps are identified.
 
